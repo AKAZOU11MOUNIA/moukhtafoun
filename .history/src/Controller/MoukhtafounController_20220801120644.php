b2b90bae61f135f5e848e7395460c3232b06f9f8
Your Name <you@example.com>
2022-08-05 13:41:08 +0200

    affichage page d acceuil

---
+++ .history/src/Controller/MoukhtafounController_20220801120644.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PersonnePerdue;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MoukhtafounController extends AbstractController
+{
+    #[Route('/', name: 'app_moukhtafoun')]
+    
+    public function index(ManagerRegistry $doctrine): Response
+    { 
+        $wls =$doctrine->getRepository(PersonnePerdue::class)->findAll();
+        return $this->render('moukhtafoun/index.html.twig',array('wls' => $wls));
+    }
+    #[Route('/details/{id}', name: 'details')]
+    public function show($id): Response
+    {
+        $pr = $this->PersonnePerdueRepository->find($id);
+        
+        return $this->render('moukhtafoun/show.html.twig', [
+            'pr' => $pr
+        ]);
+    }
+}
